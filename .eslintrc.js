d6974a0b6b26985cc0b809ed795b23d7254998a7
Genbu Hase <hasegenbu@gmail.com>
2019-11-30 15:13:39 +0900

    TypeScript環境を構築

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,12 @@ module.exports = {
 		"es6": true,
 		"browser": true
 	},
-	
+
 	"parserOptions": {
 		"ecmaVersion": 6,
 		"sourceType": "module"
 	},
-	
+
 	"rules": {
 		"semi": ["error", "always", { "omitLastInOneLineBlock": true }],
 		"semi-style": ["error", "last"],
@@ -22,5 +22,36 @@ module.exports = {
 		"prefer-spread": "warn",
 		"prefer-template": "warn",
 		"prefer-numeric-literals": "warn"
-	}
+	},
+
+	"overrides": [
+		{
+			"files": ["**/*.ts"],
+			"plugins": ["@typescript-eslint"],
+	
+			"env": {
+				"es6": true,
+				"node": true
+			},
+
+			"parser": "@typescript-eslint/parser",
+			"parserOptions": {
+				"ecmaVersion": 6,
+				"sourceType": "module",
+				"project": ""
+			},
+
+			"rules": {
+				"no-unused-vars": "off",
+				"@typescript-eslint/no-unused-vars": "error",
+				"indent": "off",
+				"@typescript-eslint/indent": "off",
+				"camelcase": "off",
+				"@typescript-eslint/camelcase": ["error", { "properties": "never" }],
+				"@typescript-eslint/no-namespace": "off",
+				"@typescript-eslint/no-parameter-properties": "off",
+				"@typescript-eslint/no-inferrable-types": "off"
+			}
+		}
+	]
 }
